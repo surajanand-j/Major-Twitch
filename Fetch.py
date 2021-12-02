920c2db0b1d0032c03d6a3d76e1e2337cbf98454
Suraj Anand <surajanand218@gmail.com>
2021-12-02 12:59:42 +0530

    Commit Added

--- Fetch.py
+++ Fetch.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
-CONNECTION_STRING = "mongodb+srv://suraj:wtcsproject@wtcs.sweig.mongodb.net/wtcsproject?retryWrites=true&w=majority"
-client = MongoClient(CONNECTION_STRING)
+import config
+client = MongoClient(config.CONNECTION_STRING)
 db=client['Twitch']
 col=db['Games']
 res=col.find_one()
---
+++ config.py
@@ -0,0 +1,3 @@
+CONNECTION_STRING = "mongodb+srv://suraj:wtcsproject@wtcs.sweig.mongodb.net/wtcsproject?retryWrites=true&w=majority"
+client_id= 'kfdicyd8nct8dwqd8vy7zrc8jn5sfw'
+client_secret= 'o77rnssqevendjf22z2sb21erif5n9'
